82291180489f41e9f41cda2163b492b7d74d3d18
Andrés Mejía <andmej@gmail.com>
2010-05-23 23:13:25 -0500

    Showing latest tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def tweet_url(author, tweet_id)
+    "http://twitter.com/#{author}/statuses/#{tweet_id}"
+  end
 end
