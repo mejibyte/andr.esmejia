49c3df716931fa33cbaf8d78d4d449bca08d4f8e
Andrés Mejía <andmej@gmail.com>
2010-05-28 20:32:31 -0500

    Adding helper for buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def button(options = {})
+    content_tag(:li, :id => options[:id]) do
+      link_to(options[:text], options[:url], :class => options[:pressed] ? "pressed" : "not-pressed")
+    end
+  end
+  
   def clear
     "<div class='clear'></div>"
   end
