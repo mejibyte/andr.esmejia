902809a0f1db0bcc804560be96820ae4f86a2e51
Andrés Mejía <andmej@gmail.com>
2010-06-16 00:16:25 -0500

    Adding :whiny => false to Uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,3 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :file, :styles => { :big => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :file, :styles => { :big => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :whiny => false
 end
