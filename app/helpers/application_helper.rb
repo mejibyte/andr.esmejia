7f99893cda877bf4c1b71c36c549047697a24d22
Andrés Mejía <andmej@gmail.com>
2010-10-10 18:44:36 +0200

    Adding my encoded email address to the Contact button.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,10 @@ module ApplicationHelper
   def clear
     "<div class='clear'></div>"
   end
+  
+  def contact_me_url
+    "&#109;&#97;&#105;&#108;&#116;&#111;&#58;%61%6e%64%72@%65%73%6d%65%6a%69%61.%63%6f%6d?" + 
+    "subject=" + CGI::escape("Hi, I just clicked on the Contact button on http://andr.esmejia.com.").gsub("+", "%20") +
+    "&body=" + CGI::escape("And I wanted to tell you that...").gsub("+", "%20")
+  end
 end
