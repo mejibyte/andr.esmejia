aa6a930aabb42af0230fb36b3dacb70a4a88080b
Andrés Mejía <andmej@gmail.com>
2013-08-18 19:12:01 -0300

    Adding meta description to home.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,20 +5,26 @@ module ApplicationHelper
       link_to(options[:text], options[:url], :class => options[:pressed] ? "pressed" : "not-pressed")
     end
   end
-  
+
+  def meta_description(some_description)
+    content_for :head do
+      tag :meta, name: :description, content: some_description.to_s
+    end
+  end
+
   def textile_to_html(textile)
      RedclothCoderay.coderay_options :line_numbers => :table
      cloth = RedCloth.new(textile)
      #cloth.extend PurpleCloth
      cloth.to_html.html_safe
   end
-  
+
   def clear
     "<div class='clear'></div>".html_safe
   end
-  
+
   def contact_me_url
-    ("&#109;&#97;&#105;&#108;&#116;&#111;&#58;%61%6e%64%72@%65%73%6d%65%6a%69%61.%63%6f%6d?" + 
+    ("&#109;&#97;&#105;&#108;&#116;&#111;&#58;%61%6e%64%72@%65%73%6d%65%6a%69%61.%63%6f%6d?" +
     "subject=" + CGI::escape("Hi, I just clicked on the Contact button on http://andr.esmejia.com.").gsub("+", "%20") +
     "&body=" + CGI::escape("And I wanted to tell you that...").gsub("+", "%20")).html_safe
   end
